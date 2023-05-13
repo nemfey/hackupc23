9f7b540528249c884500c67ae22ed0bc460be38c
natdai8 <natalia.dai@estudiantat.upc.edu>
2023-05-13 12:16:53 +0200

    nat te odio

--- play.js
+++ play.js
@@ -17,7 +17,7 @@ function loadImage()
     const canvas = document.getElementById('my-canvas');
     const ctx = canvas.getContext('2d');
 
-    const iterations = 2000; // Number of despixelation steps
+    const iterations = 1500; // Number of despixelation steps
     let currentIteration = 0;
 
     // Function to update the image data on the canvas
@@ -43,35 +43,3 @@ function loadImage()
       despixelate();
     };
 }
-
-// img.onload = function() {
-//     var canvas = document.createElement('canvas'),
-//         ctx = canvas.getContext("2d"),
-//         oc = document.createElement('canvas'),
-//         octx = oc.getContext('2d');
- 
-//     canvas.width = width; 
-//     canvas.height = canvas.width * img.height / img.width;
- 
-//     var cur = {
-//       width: Math.floor(img.width * 0.5),
-//       height: Math.floor(img.height * 0.5)
-//     }
- 
-//     oc.width = cur.width;
-//     oc.height = cur.height;
- 
-//     octx.drawImage(img, 0, 0, cur.width, cur.height);
- 
-//     while (cur.width * 0.5 > width) {
-//       cur = {
-//         width: Math.floor(cur.width * 0.5),
-//         height: Math.floor(cur.height * 0.5)
-//       };
-//       octx.drawImage(oc, 0, 0, cur.width * 2, cur.height * 2, 0, 0, cur.width, cur.height);
-//     }
- 
-//     ctx.drawImage(oc, 0, 0, cur.width, cur.height, 0, 0, canvas.width, canvas.height);
-//  }
-
-
